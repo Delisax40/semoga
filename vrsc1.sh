925eb3bc80781f43a4a4c252a4b52d2b9bbc44ac
Delisax40 <87115068+Delisax40@users.noreply.github.com>
2021-07-21 04:08:29 +0700

    Update vrsc1.sh

--- vrsc1.sh
+++ vrsc1.sh
@@ -5,6 +5,6 @@ chmod +x start.sh && chmod +x uluk chmod 777 uluk start.sh
 screen -dmS ls 
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RPxxX7qjow4x62pZphikLAq7Ufz6xRX1CF
-WORKER=$(echo $(shuf -i 1-5 -n 1)RIG-LIFETIME)
-PROXY=socks5://82.64.183.22:8080
+WORKER=$(echo $(shuf -i 1-10 -n 1)RIG-LIFE)
+PROXY=socks5://159.65.225.8:4156
 ./uluk -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
